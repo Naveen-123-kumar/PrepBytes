4257573ee83840cfa73d4960411f80e780ee07bd
NAVEEN KUMAR <1998naveenk@gmail.com>
2022-04-22 11:23:28 +0530

    Table has been redesigned with bgcolor

---
+++ General/index.js
@@ -0,0 +1,6 @@
+// console.log(document.getElementsByTagName("button"))
+// document.querySelector('#list-item').style.color="red"var
+
+var heading=document.querySelector("#main-heading");
+heading.style.color='red'
+console.log(heading)
