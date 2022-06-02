5e4a387e708f86a5a9234039729bfef9b5e9d326
NAVEEN KUMAR <1998naveenk@gmail.com>
2022-06-02 15:20:32 +0530

    All

--- ElevationAcadmy/DailyClasses/JSDailyclasses/D-01.06/day3.js
+++ ElevationAcadmy/DailyClasses/JSDailyclasses/D-01.06/day3.js
@@ -13,7 +13,7 @@ console.log(b,c);
 //Logical operators
 let f=0;
 let g=3;
-console.log(f&& g);//if the first variable is non zero ,the the second variable is the o/p if the first varible is zero /false then second variable will neber be checked and the o/p will be 0/false respectively.
+console.log(f && g);//if the first variable is non zero ,the the second variable is the o/p if the first varible is zero /false then second variable will never be checked and the o/p will be 0/false respectively.
 console.log(f || g)//if the first variable is non zero ,the the first variable is the o/p if the first varible is zero /false then second variable will be checked and the o/p will be second respectively.
 //Negation
 console.log(!(f>0));//false always gives boolean values.A number that is non-zero is considered as true by default and a negation on true is suppose to give u false.negation is always going to give u values in true/false only.
