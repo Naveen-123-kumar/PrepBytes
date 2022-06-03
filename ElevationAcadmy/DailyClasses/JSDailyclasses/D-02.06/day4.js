cb671e7a04e98519ab83efbdfd13b2bbc5c56064
NAVEEN KUMAR <1998naveenk@gmail.com>
2022-06-03 21:24:55 +0530

    Pushing code to git on 03-06-2022

---
+++ ElevationAcadmy/DailyClasses/JSDailyclasses/D-02.06/day4.js
@@ -0,0 +1,96 @@
+// let num=40;
+// if(num>=40){
+//     console.log('Pass');
+// }
+// else{
+//     console.log('fail');
+// }
+//if else block -if the condition is true then the if block is executed and if the condition isnot true then the else block will be executed.The else block never checks for any condition.
+// function test(num){
+//     if(num >= 40){
+//         console.log('pass');
+//     }
+//     else{
+//         console.log('fail');
+//     }
+// }
+// test(50);
+// function num(age){
+//     if(age<18){
+//         console.log('child');
+//     }
+//     else if(age>=18 && age<60){
+//         console.log('adult');
+//     }
+//     else{
+//         console.log('senior citizen');
+//     }
+// }
+// num(45);
+function addition(a,b,c,d){
+    let result=a+b+c+d;
+    // console.log(result);
+    return result;
+
+}
+function substraction(a,b){
+    let result1=a-b;
+    return result1;
+}
+
+
+function multiply(a,b){
+let result=a*b;
+// console.log(result)
+return result;
+
+}
+// let x=multiply(2,4)
+console.log(multiply(2,4));
+
+multiply(50,10)
+
+
+
+let variableName=substraction(18,4);
+// console.log(variableName)
+
+
+
+
+console.log(substraction(10,5))
+addition(4,2,4,8);
+addition(20,30,50,40);
+// let a=39;
+// let b=20;
+// let result=a+b;
+// console.log(result);
+// console.log(a+b);
+// let c=50;
+// let d=40;
+// let n1
+// let n2
+// let n3
+// let result1=c+d+n1+n2+n3+n4;
+// console.log(result1);
+
+// function checkEven(number){
+//     if(number%2==0){
+// console.log('It is even');
+//     }
+//     else{
+// console.log('Odd')
+//     }
+// }
+// checkEven(10);
+
+
+function checkAge(age){
+    if(age>=18){
+        console.log(true);
+    }
+    else{
+        console.log(false);
+    }
+}
+checkAge(18);
---
+++ ElevationAcadmy/DailyClasses/JSDailyclasses/D-03.06/day5.js
@@ -0,0 +1,38 @@
+// let number=2;
+// switch(number){
+//     case 1:
+//         console.log("I am number 1");
+//         break;
+//     case 2:
+//         console.log("I am number 2");
+//         break;
+//     case 3:
+//         console.log("I am number 3");
+//         break;
+//     case 4:
+//         console.log("I am number 4");
+//         break;
+//     case 5:
+//         console.log("I am number 5");
+//         break;
+//     default:
+//         console.log("I am number greater than 5");
+//         break;
+// }
+
+//for loop is used to iterate any statement for fixed number of times
+var n=5;
+
+for(let i=1;i<=10;i++){
+    console.log('3 *', i ,'=', 3*i);
+}
+let i=0;
+while(i<10){
+    console.log('print the value of i ',i);
+    i++;
+}
+let j=0;
+do{
+    console.log(j);
+    j++;
+}while(j<5);
